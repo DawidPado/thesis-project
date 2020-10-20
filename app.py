ef416259841242b713461bb55f3ea46647c10806
Xardas7 <taif12340@gmail.com>
2020-10-20 12:08:23 +0200

    login template update

--- app.py
+++ app.py
@@ -1,74 +1,75 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_restful import Api, Resource, reqparse
 import json
 
 app = Flask(__name__)
+app.secret_key = 'random string'
 api = Api(app)
 parser = reqparse.RequestParser()
-""""
-STUDENTS = {
-  '1': {'name': 'Mark', 'age': 23, 'spec': 'math'},
-  '2': {'name': 'Jane', 'age': 20, 'spec': 'biology'},
-  '3': {'name': 'Peter', 'age': 21, 'spec': 'history'},
-  '4': {'name': 'Kate', 'age': 22, 'spec': 'science'},
-}
 
-class StudentList(Resource):
-    def get(self):
-        return STUDENTS
-    def post(self):
-        parser.add_argument("name")
-        parser.add_argument("age")
-        parser.add_argument("spec")
-        args = parser.parse_args()
-        student_id = int(max(STUDENTS.keys())) + 1
-        student_id = '%i' % student_id
-        STUDENTS[student_id] = {
-            "name": args["name"],
-            "age": args["age"],
-            "spec": args["spec"],
-        }
-        return STUDENTS[student_id], 201
-
-api.add_resource(StudentList, '/students/')
+"""
+@app.route('/login/', methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+       parser.add_argument("username")
+       parser.add_argument("password")
+       args = parser.parse_args()
+       status = {}
+       with open('json/file.json', 'r') as myfile:
+           data = myfile.read()
+       # parse file
+       obj = json.loads(data)
+       for i in obj:
+           if obj[i]['username'] == args["username"]:
+               if obj[i]['password'] == args["password"]:
+                   status = {"status": "success"}
+                   return status
+               else:
+                   status = {"status": "fail"}
+                   return status
+       status = {"status": "fail"}
+       return status
+   else:
+      return render_template('login.html')
 
 """
 
-
-#@app.route('/login',methods = ['POST', 'GET'])
-
-class Login(Resource):
-    def get(self):
-        return render_template('login.html')
-
-    def post(self):
-        parser.add_argument("username")
-        parser.add_argument("password")
-        args = parser.parse_args()
-        status = {}
-        with open('json/file.json', 'r') as myfile:
-            data = myfile.read()
-        # parse file
-        obj = json.loads(data)
-        for i in obj:
-            if obj[i]['username'] == args["username"]:
-                if obj[i]['password'] == args["password"]:
-                    status = {"status": "success"}
-                    return status
-                else:
-                    status = {"status": "fail"}
-                    return status
-        status = {"status": "fail"}
-        return status
-
-
-api.add_resource(Login, '/login/')
+@app.route('/login/', methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+       parser.add_argument("username")
+       parser.add_argument("password")
+       args = parser.parse_args()
+       status = {}
+       with open('json/file.json', 'r') as myfile:
+           data = myfile.read()
+       # parse file
+       obj = json.loads(data)
+       for i in obj:
+           if obj[i]['username'] == args["username"]:
+               if obj[i]['password'] == args["password"]:
+                   session['username'] = args["username"]
+                   status = {"status": "success"}
+                   return status
+               else:
+                   status = {"status": "fail"}
+                   return status
+       status = {"status": "fail"}
+       return status
+   else:
+      return render_template('login.html')
 
 
 @app.route('/')
-def hello_world():
-    return render_template('login.html')
+def start():
+    if 'username' in session:
+        username = session['username']
+        return 'Logged in as ' + username + '<br>' + \
+               "<b><a href = '/logout'>click here to log out</a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+       "click here to log in</b></a>"
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
