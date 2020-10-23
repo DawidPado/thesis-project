e695f2b91a3ce7e6c3e6720ad13344721a64e586
Xardas7 <taif12340@gmail.com>
2020-10-23 12:38:38 +0200

    rand data and elasticsearch

--- data_gen.py
+++ data_gen.py
@@ -1,18 +1,21 @@
 import random
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['http://localhost:9200/'])
+es = Elasticsearch()
 
-url = 'http://localhost:9200/energy/_dev/'
-
+#url = 'http://localhost:9200/energy/_dev/'
+#serach='http://localhost:9200/energy/_search'
 
 
 # data time format 2015-01-01T12:11:30Z
-data = "2020-10-22"
+data = "2020-10-24"
 hh="00"
 mm="00"
 ss="00"
+message=''
 messages = []
+
+
 for i in range(24):
     if i < 10:
         hh="0"+ str(i)
@@ -34,6 +37,32 @@ for i in range(24):
                 message = message + " }"
             else :
                 message = message + ", "
-       # x = requests.post(url, data=message)
-        # messages.append(message)
-res = es.create('energy',body=message)
+       # messages.append(message)
+        rex = es.index(index='energy', body=message)
+
+
+for i in range(24):
+    if i < 10:
+        hh="0"+ str(i)
+    else:
+        hh=str(i)
+    for j in range(60):
+        if j < 10:
+            mm="0"+str(j)
+        else:
+            mm=str(j)
+        date=data+'T'+hh+':'+mm+':'+ss+'Z'
+        message ="{ \"timestamp\":\""+ date+"\","
+        for k in range (23):
+            if k == 0:
+                continue
+            n = random.randrange(0, 1000)
+            message = message + " \"S"+str(k)+"\": " +str(n)
+            if k==22:
+                message = message + " }"
+            else :
+                message = message + ", "
+       # messages.append(message)
+        rex = es.index(index='traffic', body=message)
+
+#res = es.search(index='energy')
