74be4286922c2e29775cd0ff234de960b64e2373
Xardas7 <taif12340@gmail.com>
2020-11-03 17:38:46 +0100

    logout fix

--- static/dashboard/js/main.js
+++ static/dashboard/js/main.js
@@ -1,18 +1,14 @@
 //logout
 $('#logout').on('click', function(e) {
 		e.preventDefault();
-	var x =	 swal("Do you really want to logout?", {
+	swal("Do you really want to logout?", {
 		icon: "warning",
 		dangerMode: true,
   		buttons: true,
 		confirmButtonText: 'Logout',
 		timer: 10000,
-		});
-
-	console.log(x.value)
-	/*	if(confirm("Do you really want to logout?")) {
-
-
+		}).then((value) => {
+		if(value===true){
 			$.ajax({
 				url: 'http://127.0.0.1:5000/logout',
 				method: 'POST',
@@ -23,5 +19,6 @@ $('#logout').on('click', function(e) {
 					console.log(err);
 				}
 			});
-		}*/
+		}
+	});
 	});
